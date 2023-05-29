28fd204787b99f7780296a62241440856233d165
yurioneix <yurioneix18@gmail.com>
2023-05-29 19:38:27 -0300

    Corrige erros do linter

--- src/database/models/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var sequelize_1 = require("sequelize");
-var config = require("../config/database");
-var db = new sequelize_1.Sequelize(config);
-exports["default"] = db;
